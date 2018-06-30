782d45afa322b3413b493dfbc4f901aa643c8df8
xfg <853299701@qq.com>
2018-06-30 17:52:28 +0800

    p

--- FWPopupView/FWPopupView/FWPopupView/FWSheetView.swift
+++ FWPopupView/FWPopupView/FWPopupView/FWSheetView.swift
@@ -210,20 +210,11 @@ extension FWSheetView {
             tmpIndex += 1
         }
         
-        // 目前先用该方法来适配iPhoneX，以下方法发布时会有问题
-        if UIScreen.main.bounds.height == 812 {
-            self.frame.size.height = btnContrainerView.frame.maxY + self.vProperty.buttonHeight + property.cancelBtnMarginTop + 34
-        }else {
+        if #available(iOS 11.0, *) {
+            self.frame.size.height = btnContrainerView.frame.maxY + self.vProperty.buttonHeight + property.cancelBtnMarginTop + FWPopupWindow.sharedInstance.safeAreaInsets.bottom
+        } else {
             self.frame.size.height = btnContrainerView.frame.maxY + self.vProperty.buttonHeight + property.cancelBtnMarginTop
         }
-        
-        /**
-         if #available(iOS 11.0, *) {
-            self.frame.size.height = btnContrainerView.frame.maxY + self.vProperty.buttonHeight + property.cancelBtnMarginTop + FWPopupWindow.sharedInstance.safeAreaInsets.bottom
-         } else {
-            self.frame.size.height = btnContrainerView.frame.maxY + self.vProperty.buttonHeight + property.cancelBtnMarginTop
-         }
-         */
     }
 }
 
