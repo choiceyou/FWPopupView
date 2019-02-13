6213a260e25c7f010cbf81d20d6658d21bf0555a
xfg <853299701@qq.com>
2019-02-13 18:09:55 +0800

    p

--- FWPopupView/FWPopupViewTests/FWPopupViewTests.swift
+++ FWPopupView/FWPopupViewTests/FWPopupViewTests.swift
@@ -2,8 +2,8 @@
 //  FWPopupViewTests.swift
 //  FWPopupViewTests
 //
-//  Created by xfg on 2018/3/19.
-//  Copyright © 2018年 xfg. All rights reserved.
+//  Created by xfg on 2019/2/13.
+//  Copyright © 2019 xfg. All rights reserved.
 //
 
 import XCTest
@@ -11,26 +11,45 @@ import XCTest
 
 class FWPopupViewTests: XCTestCase {
     
+    var popupView: FWPopupView!
+
     override func setUp() {
-        super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        popupView = FWPopupView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width * 0.5, height: UIScreen.main.bounds.height * 0.5))
+        let vProperty = FWPopupViewProperty()
+        vProperty.popupCustomAlignment = .topCenter
+        vProperty.popupAnimationType = .frame
+        vProperty.maskViewColor = UIColor(white: 0, alpha: 0.5)
+        vProperty.touchWildToHide = "1"
+        vProperty.popupViewEdgeInsets = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
+        vProperty.animationDuration = 0.2
+        popupView.vProperty = vProperty
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+        popupView = nil
     }
-    
+
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
+    func testShow() {
+        XCTAssertNoThrow(popupView.show())
+    }
+    
+    func testHide() {
+        
+    }
+
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-    
+
 }
--- FWPopupView/FWPopupViewUITests/FWPopupViewUITests.swift
+++ FWPopupView/FWPopupViewUITests/FWPopupViewUITests.swift
@@ -2,35 +2,48 @@
 //  FWPopupViewUITests.swift
 //  FWPopupViewUITests
 //
-//  Created by xfg on 2018/3/19.
-//  Copyright © 2018年 xfg. All rights reserved.
+//  Created by xfg on 2019/2/13.
+//  Copyright © 2019 xfg. All rights reserved.
 //
 
 import XCTest
 
 class FWPopupViewUITests: XCTestCase {
-        
+
     override func setUp() {
-        super.setUp()
-        
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        
+
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
+
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
     }
-    
+
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let app = XCUIApplication()
+        let tablesQuery = app.tables
+        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Alert - 单个按钮"]/*[[".cells.staticTexts[\"Alert - 单个按钮\"]",".staticTexts[\"Alert - 单个按钮\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        let button = app.buttons["知道了"]
+        button.tap()
+        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Alert - 两个按钮"]/*[[".cells.staticTexts[\"Alert - 两个按钮\"]",".staticTexts[\"Alert - 两个按钮\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        let button2 = app.buttons["确定"]
+        button2.tap()
+        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Alert - 两个按钮（修改参数）"]/*[[".cells.staticTexts[\"Alert - 两个按钮（修改参数）\"]",".staticTexts[\"Alert - 两个按钮（修改参数）\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        button2.tap()
+        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Alert - 多个按钮"]/*[[".cells.staticTexts[\"Alert - 多个按钮\"]",".staticTexts[\"Alert - 多个按钮\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
     }
-    
+
 }
